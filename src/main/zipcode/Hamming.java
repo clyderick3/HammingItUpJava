cc15a312c637abf55decd2f7e49ca5ccbb5ae33d
Clyde Broderick <clyderick3@gmail.com>
2022-04-16 13:52:57 -0400

    Added new instance variables for getHammingDistance.

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -3,8 +3,12 @@ package zipcode;
 public class Hamming {
     int sLength;
     int s1Length;
+    String thing = "";
+    String thing1 = "";
 
     public Hamming(String s, String s1) {
+        thing = s;
+        thing1 = s1;
         sLength = s.length();
         s1Length = s1.length();
         if (sLength != s1Length){
@@ -17,7 +21,7 @@ public class Hamming {
         // are different from their equivalent in the other string.
         int count = 0;
         for (int i = 0; i < s1Length; i++){
-            if (s)
+
 
         }
 
