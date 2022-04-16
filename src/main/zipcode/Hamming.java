8d46354e47d3a9e47095da94de6f2a1fc9e0b173
Clyde Broderick <clyderick3@gmail.com>
2022-04-16 13:28:20 -0400

    Started HammingItUp.

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -2,9 +2,16 @@ package zipcode;
 
 public class Hamming {
     public Hamming(String s, String s1) {
+        int sLength = s.length();
+        int s1Length = s1.length();
+        if (sLength != s1Length){
+            throw new IllegalArgumentException("The two strands MUST be of equal length!")
+        }
     }
 
     public int getHammingDistance() {
+        //Comparing two DNA strands and counting how many of the nucleotides
+       // are different from their equivalent in the other string.
         return -1;
     }
 }
