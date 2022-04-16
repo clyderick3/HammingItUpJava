695703d0b8b5f42ee1f2cdd86391df01062a65c9
Clyde Broderick <clyderick3@gmail.com>
2022-04-16 13:51:09 -0400

    Reintroducing instance variables.

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,9 +1,12 @@
 package zipcode;
 
 public class Hamming {
+    int sLength;
+    int s1Length;
+
     public Hamming(String s, String s1) {
-        int sLength = s.length();
-        int s1Length = s1.length();
+        sLength = s.length();
+        s1Length = s1.length();
         if (sLength != s1Length){
             throw new IllegalArgumentException("The two strands MUST be of equal length!")
         }
@@ -12,6 +15,11 @@ public class Hamming {
     public int getHammingDistance() {
         //Comparing two DNA strands and counting how many of the nucleotides
         // are different from their equivalent in the other string.
+        int count = 0;
+        for (int i = 0; i < s1Length; i++){
+            if (s)
+
+        }
 
         return -1;
     }
